5109dc7592a813ffca3bd3723828ec0a971de3a6
Szaszuge <78954822+Szaszuge@users.noreply.github.com>
2025-02-03 10:29:12 +0100

    navbar component + icons

--- OroszFront/src/app/app.component.ts
+++ OroszFront/src/app/app.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MenubarModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'OroszFront';
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+          label: 'Order',
+          icon: 'pi pi-cart-plus',
+      },
+        {
+            label: 'Login',
+            icon: 'pi pi-sign-in'
+        },
+        {
+            label: 'Register',
+            icon: 'pi pi-user-plus'
+        },
+        {
+            label: 'Admin',
+            icon: 'pi pi-user'
+        }
+    ]
+}
 }
